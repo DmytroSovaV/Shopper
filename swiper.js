46342f9d02f7f74c73e4150192abe9fc6ff18488
DmytroSovaV <ikrosua@gmail.com>
2025-11-20 20:52:44 +0200

    first commit

---
+++ swiper.js
@@ -0,0 +1,35 @@
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//    slidesPerView: 3,
+//   spaceBetween: 90,
+// });
+
+// const swiper2 = new Swiper('.swiper2', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//    slidesPerView: 1,
+//   spaceBetween: 55,
+// });
