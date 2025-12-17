16af087940948e038a32df71319cdf51b6b7f4aa
DmytroSovaV <ikrosua@gmail.com>
2025-12-17 22:53:28 +0200

    adaptive

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-document.querySelector(".top-sellers-section__category")
-  .addEventListener("click", e => {
-    if (e.target.tagName === "BUTTON") {
-      // видаляємо клас у всіх кнопок і додаємо тільки тій, на яку клікнули
-      document.querySelectorAll(".top-sellers-section__category button")
-              .forEach(btn => btn.classList.remove("active-category"));
-      e.target.classList.add("active-category");
-    }
-  });
+//  document.querySelector(".top-sellers-section__category")
+//    .addEventListener("click", e => {
+//      if (e.target.tagName === "BUTTON") {
+//        // видаляємо клас у всіх кнопок і додаємо тільки тій, на яку клікнули
+//        document.querySelectorAll(".top-sellers-section__category button")
+//                .forEach(btn => btn.classList.remove("active-category"));
+//        e.target.classList.add("active-category");
+//      }
+//    });
 
-  document.querySelector(".trending-section__category")
-  .addEventListener("click", e => {
-    if (e.target.tagName === "BUTTON") {
-      // видаляємо клас у всіх кнопок і додаємо тільки тій, на яку клікнули
-      document.querySelectorAll(".trending-section__category button")
-              .forEach(btn => btn.classList.remove("active-category"));
-      e.target.classList.add("active-category");
-    }
-  });
+//    document.querySelector(".trending-section__category")
+//    .addEventListener("click", e => {
+//      if (e.target.tagName === "BUTTON") {
+//        // видаляємо клас у всіх кнопок і додаємо тільки тій, на яку клікнули
+//        document.querySelectorAll(".trending-section__category button")
+//                .forEach(btn => btn.classList.remove("active-category"));
+//        e.target.classList.add("active-category");
+//      }
+//    });
 
-// const blackBtn = document.getElementById('black');
-// const images = document.getElementsByClassName('11');
-// console.log(blackBtn);
-// console.dir(images);
+// //  const blackBtn = document.getElementById('black');
+// //  const images = document.getElementsByClassName('11');
+// //  console.log(blackBtn);
+// //  console.dir(images);
 
 
-// blackBtn.addEventListener("click", function (){
-//   images.style.backgroundColor = "red";
-// })
+// // blackBtn.addEventListener("click", function (){
+// //    images.style.backgroundColor = "red";
+// //  })
 
 
 
--- swiper.js
+++ swiper.js
@@ -13,7 +13,34 @@ const swiper = new Swiper('.swiper', {
 
   // And if we need scrollbar
    slidesPerView: 3,
-  spaceBetween: 90,
+    spaceBetween: 30,
+    breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 93
+    },
+    // when window width is >= 640px
+    640: {
+        slidesPerView: 2,
+        spaceBetween: 93
+    },
+    1024: {
+        slidesPerView: 2,
+        spaceBetween: 93
+        
+    },
+    1100: {
+        slidesPerView: 3,
+        spaceBetween: 90
+        
+    }
+  }
 });
 
 const swiper2 = new Swiper('.swiper2', {
@@ -31,5 +58,34 @@ const swiper2 = new Swiper('.swiper2', {
 
   // And if we need scrollbar
    slidesPerView: 1,
-  spaceBetween: 55,
+    spaceBetween: 55,
+//    slidesPerView: 3,
+//     spaceBetween: 30,
+//     breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 1,
+//       spaceBetween: 93
+//     },
+//     // when window width is >= 640px
+//     640: {
+//         slidesPerView: 1,
+//         spaceBetween: 93
+//     },
+//     1024: {
+//         slidesPerView: 1,
+//         spaceBetween: 93
+        
+//     },
+//     1100: {
+//         slidesPerView: 1,
+//         spaceBetween: 90
+        
+//     }
+//   }
 });
